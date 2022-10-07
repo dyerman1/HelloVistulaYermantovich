4faedd5621d8839348c759463f1a093aa76e63ab
dyerman1 <dyerman1@stu.vistula.edu.pl>
2022-10-07 12:19:02 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldYermantovich/HelloWorld/Yermantovich.java
@@ -0,0 +1,7 @@
+package pl.vistula.helloworldYermantovich.HelloWorld;
+
+public class Yermantovich {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
